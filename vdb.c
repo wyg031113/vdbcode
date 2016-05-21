cb5bc6a70ae070f36ed1690203f329797e6dcdbb
root <root@wyg-vpc.(none)>
2016-05-21 11:48:10 +0800

    use element_pp_pow speed up

--- vdb.c
+++ vdb.c
@@ -105,28 +105,39 @@ int vdb_setup(struct setup_struct *ss, int q, int argc, char *argv[])
 
 	//compute hi
 //	printf("Beging compute hi\n");
+
+	element_pp_t gpp;
+	element_pp_init(gpp, pp->g);
 	for(i = 0; i < q; i++)	
 	{
 		//printf("i = %d\n", i);
 		element_init_G1(pp->hi[i], pp->pairing);
 		element_init_Zr(z[i], pp->pairing);			//let z in ZZr
 		element_random(z[i]);
-		element_pow_zn(pp->hi[i], pp->g, z[i]);	
+//		element_pow_zn(pp->hi[i], pp->g, z[i]);	
+		element_pp_pow_zn(pp->hi[i], z[i], gpp);
 	//	element_printf("z=%B\n", z);
 	//	element_printf("h%d=%B\n\n", i, pp->hi[i]);
 	}
+	element_pp_clear(gpp);
 	for(i = 0; i < q; i++)
+	{
+		element_pp_t gpp;
+		element_pp_init(gpp, pp->hi[i]);
 		for(j = i+1; j < q; j++)
 		{
 			
 			element_init_G1(pp->hij[i*q+j], pp->pairing);
 			element_init_G1(pp->hij[j*q+i], pp->pairing);
 
-			element_pow_zn(pp->hij[i*q+j], pp->hi[i], z[j]);	
+			//element_pow_zn(pp->hij[i*q+j], pp->hi[i], z[j]);	
+			element_pp_pow_zn(pp->hij[i*q+j], z[j], gpp);
 			element_set(pp->hij[j*q+i], pp->hij[i*q+j]);
 
 			//element_printf("h[%d][%d] =%B\n\n", i, j, pp->hij[i*q+j]);
 		}
+		element_pp_clear(gpp);
+	}
 
 
 	ss->PK.pp =  ss->S.pp = pp;	
@@ -161,7 +172,7 @@ out5:
 int main(int argc, char *argv[])
 {
 	struct setup_struct ss;
-	vdb_setup(&ss, 50, argc, argv);
+	vdb_setup(&ss, 1000, argc, argv);
 //	showss(&ss);
 	return 0;
 }
